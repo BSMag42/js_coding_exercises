2a62d0ae9625528362e11fc801983fadbdbb0641
bsmagennis42 <bsmagennis42@gmail.com>
2020-12-04 16:48:26 +0000

    added another test to sumMultiples

--- test/exercise006.test.js
+++ test/exercise006.test.js
@@ -4,14 +4,17 @@ const {
 
 describe("sumMultiples", () => {
   test("receive an array of numbers and should return the sum of any numbers which are a multiple of 3 or 5", () => {
-    const multiples = [1,3,5,15];     
-    
+    const multiples = [1,3,5,15];        
     expect(sumMultiples(multiples)).toEqual(23);
   });
 
   test("receive an array return the sum of any numbers return 0 if there are no multiple of 3 or 5", () => {
     const multiples = [1,4,7,43];
     expect(sumMultiples(multiples)).toEqual(0);
+  });
+
+  test("receive an array return the sum of any numbers multiple of 3 or 5", () => {
+    expect(sumMultiples([1,7,15,33])).toEqual(48);
   });
 });
 
