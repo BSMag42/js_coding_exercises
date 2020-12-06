801c0323c049b620a81f6735b4451b7530b20121
bsmagennis42 <bsmagennis42@gmail.com>
2020-12-06 16:38:42 +0000

    initialise sumDigits so returns 0 if array empty

--- test/exercise007.test.js
+++ test/exercise007.test.js
@@ -7,5 +7,9 @@ const {
       const multiples = [1,3,5,15];        
       expect(sumDigits(multiples)).toEqual(24);
     });
+    test("receive an empty array of numbers and should return 0", () => {
+        const multiples = [];        
+        expect(sumDigits(multiples)).toEqual(0);
+      });
   
   });
