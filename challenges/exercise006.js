861ce170b407a424cb60a4705f48ad0585175d4b
bsmagennis42 <bsmagennis42@gmail.com>
2020-12-06 13:18:00 +0000

    changed formatting to be more JavaScript standard

--- challenges/exercise006.js
+++ challenges/exercise006.js
@@ -56,16 +56,14 @@ const getComplementaryDNA = str => {
  */
 const isItPrime = n => {
   if (n === undefined) throw new Error("n is required");
-  if(n <= 2)
-  {
+  if(n <= 2){
     return true;
   }
-  else
-  {
-    for(let i = 2; i<n-1; i++)
-    {
-      if( n % i === 0)
+  else{
+    for(let i = 2; i<n-1; i++){
+      if( n % i === 0){
         return false;
+      }
     }
   }
 };
