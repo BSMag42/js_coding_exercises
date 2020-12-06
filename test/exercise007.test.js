8f59e7508d3712254385e1e21eb646c1d3ff2c66
bsmagennis42 <bsmagennis42@gmail.com>
2020-12-06 17:03:35 +0000

    passed createRange - worked out I need .toEqual for array comparison

--- test/exercise007.test.js
+++ test/exercise007.test.js
@@ -1,8 +1,8 @@
 const {
-    sumDigits
+    sumDigits, createRange
   } = require("../challenges/exercise007");
   
-  describe("sumDigits", () => {
+describe("sumDigits", () => {
     test("receive an array of numbers and should return the sum of numbers", () => {
       const multiples = [1,3,5,15];        
       expect(sumDigits(multiples)).toEqual(24);
@@ -12,4 +12,12 @@ const {
         expect(sumDigits(multiples)).toEqual(0);
       });
   
-  });
+ });
+
+fdescribe("createRange", () => {
+     test("create an array start = 3, end = 11 step = 2 the result: [3, 5, 7, 9, 11]", () => {
+        expect(createRange(3,11,2)).toEqual([3, 5, 7, 9, 11]);
+     })
+ })
+
+ 
