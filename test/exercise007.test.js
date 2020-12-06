41b96180371e0a3b3e406b9f4b04567e24f8afa2
bsmagennis42 <bsmagennis42@gmail.com>
2020-12-06 17:09:00 +0000

    createRange works with default step

--- test/exercise007.test.js
+++ test/exercise007.test.js
@@ -16,8 +16,17 @@ describe("sumDigits", () => {
 
 fdescribe("createRange", () => {
      test("create an array start = 3, end = 11 step = 2 the result: [3, 5, 7, 9, 11]", () => {
-        expect(createRange(3,11,2)).toEqual([3, 5, 7, 9, 11]);
-     })
+        expect(createRange(3,11,2)).toEqual([3, 5, 7, 9, 11]);        
+     });
+
+     test("create an array start = 0, end = 20 step = 5", () => {
+         expect(createRange(0,20,5)).toEqual([0,5,10,15,20]);
+     });
+
+     test("create an array start 1, end 5 no step use default", () => {
+        expect(createRange(1,5)).toEqual([1,2,3,4,5]);
+    })
+
  })
 
  
